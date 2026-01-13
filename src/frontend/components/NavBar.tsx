ebff03f26da6ef72ee2873be7a04d5c41e5239ea
4alllka1 <4alllka2442@gmail.com>
2026-01-13 13:19:44 +0300

    Add NavBar profile button, remove text from logo on small screens

--- src/frontend/components/NavBar.tsx
+++ src/frontend/components/NavBar.tsx
@@ -1,7 +1,24 @@
-import { ChartColumnStacked, Heart, Home, LogIn } from "lucide-react";
+import { ChartColumnStacked, Heart, Home, LogIn, User } from "lucide-react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { API_URL } from "../api";
 
 function NavBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  async function checkIsLoggedIn() {
+    const response = await fetch(`${API_URL}/api/users/me`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+
+    if (response.ok) {
+      setIsLoggedIn(true);
+    }
+  }
+
+  checkIsLoggedIn();
+
   const baseLinkStyle =
     "px-4 py-3 rounded-xl flex gap-2 items-center justify-center transition duration-500";
   const activeLinkStyles = "bg-white text-primary-600 font-bold";
@@ -14,14 +31,16 @@ function NavBar() {
       : `${baseLinkStyle} ${inactiveLinkStyles}`;
   };
   return (
-    <nav className="top-0 w-full h-16 bg-gradient-to-r to-primary-500 from-secondary-500 flex">
+    <nav className="top-0 w-full h-16 bg-gradient-to-r to-primary-500 from-secondary-500 flex px-2">
       <div className="container mx-auto h-full flex items-center justify-center">
         {/* Логотип */}
         <NavLink to="/" className="flex items-center h-full gap-3">
           <div className="h-10 w-10 border border-2 border-white rounded-xl flex items-center justify-center bg-gradient-to-br from-primary-300 to-secondary-500 text-2xl font-bold text-white/90">
             Т
           </div>
-          <div className="text-white text-xl font-bold">Трекер привычек</div>
+          <div className="text-white text-xl font-bold hidden md:block">
+            Трекер привычек
+          </div>
         </NavLink>
       </div>
 
@@ -40,18 +59,32 @@ function NavBar() {
         </NavLink>
       </div>
 
-      <div className="container flex items-center justify-center">
-        <NavLink
-          to="/login"
-          className="group py-3 px-5 rounded-xl border border-white/40 bg-white font-bold text-white bg-white/20 hover:bg-white/30 flex items-center justify-center gap-2 translate-all duration-400"
-        >
-          Войти
-          <LogIn
-            className="group-hover:translate-x-0.5 translate-all duration-300"
-            size={20}
-          />
-        </NavLink>
-      </div>
+      {!isLoggedIn && (
+        <div className="container flex items-center justify-center">
+          <NavLink
+            to="/login"
+            className="group py-3 px-5 rounded-xl border border-white/40 bg-white font-bold text-white bg-white/20 hover:bg-white/30 flex items-center justify-center gap-2 translate-all duration-400"
+          >
+            Войти
+            <LogIn
+              className="group-hover:translate-x-0.5 translate-all duration-300"
+              size={20}
+            />
+          </NavLink>
+        </div>
+      )}
+
+      {isLoggedIn && (
+        <div className="container flex items-center justify-center">
+          <NavLink
+            to="/profile"
+            className="group py-3 px-5 rounded-xl border border-white/40 bg-white font-bold text-white bg-white/20 hover:bg-white/30 flex items-center justify-center gap-2 translate-all duration-400"
+          >
+            Профиль
+            <User className="" size={20} />
+          </NavLink>
+        </div>
+      )}
     </nav>
   );
 }
